5fe93eb52dd6e891710f27212757be8f920d2de5
Eduardo26 <jguevaral@uni.pe>
2023-06-05 17:44:30 -0500

    UI Components

---
+++ components/Box.tsx
@@ -0,0 +1,23 @@
+import { twMerge } from "tailwind-merge";
+
+type Props = {
+  children: React.ReactNode;
+  className?: string;
+};
+
+const Box = ({ children, className }: Props) => {
+  return (
+    <div
+      className={twMerge(
+        `
+    bg-neutral-900 rounded-lg h-fit w-full
+  `,
+        className
+      )}
+    >
+      {children}
+    </div>
+  );
+};
+
+export default Box;
